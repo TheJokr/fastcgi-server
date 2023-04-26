d66c81fb5af3edff997799ba8994cf7cfd28fb74
TheJokr <leo@bloecher.dev>
2023-04-26 22:50:27 +0200

    Add tests for ProtocolVariables set

--- src/protocol/vars.rs
+++ src/protocol/vars.rs
@@ -36,3 +36,64 @@ impl ProtocolVariables {
         )
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn no_flag_overlap() {
+        let mut seen = ProtocolVariables::empty();
+        for f in ProtocolVariables::all() {
+            assert!(!seen.intersects(f), "{} overlaps with {}", f.0, seen.0);
+            seen.insert(f);
+        }
+    }
+
+    #[test]
+    fn roundtrip() -> Result<(), ProtocolError> {
+        for (name, f) in ProtocolVariables::all().iter_names() {
+            assert_eq!(ProtocolVariables::parse_name(name.as_bytes())?, f);
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn parse_spec() -> Result<(), ProtocolError> {
+        assert_eq!(
+            ProtocolVariables::parse_name(b"FCGI_MAX_CONNS")?,
+            ProtocolVariables::FCGI_MAX_CONNS,
+        );
+        assert_eq!(
+            ProtocolVariables::parse_name(b"FCGI_MAX_REQS")?,
+            ProtocolVariables::FCGI_MAX_REQS,
+        );
+        assert_eq!(
+            ProtocolVariables::parse_name(b"FCGI_MPXS_CONNS")?,
+            ProtocolVariables::FCGI_MPXS_CONNS,
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn parse_unk() {
+        const UNK_VAR: &str = "Atä w_3tFA-Es^Ü2";
+        match ProtocolVariables::parse_name(UNK_VAR.as_bytes()) {
+            Ok(v) => assert!(false, "parsed {UNK_VAR:?} as {}", v.0),
+            Err(ProtocolError::UnknownVariable(s)) => assert_eq!(s.as_ref(), UNK_VAR),
+            Err(e) => assert!(false, "returned unexpected error {e:#?}"),
+        }
+    }
+
+    #[test]
+    fn parse_invalid() {
+        const INVALID_VAR: &[u8] = b"ASg w-f#32\xFE \xFF+_a+";
+        const REPL_REPR: &str = "ASg w-f#32� �+_a+";
+        match ProtocolVariables::parse_name(INVALID_VAR) {
+            Ok(v) => assert!(false, "parsed {REPL_REPR:?} as {}", v.0),
+            Err(ProtocolError::UnknownVariable(s)) => assert_eq!(s.as_ref(), REPL_REPR),
+            Err(e) => assert!(false, "returned unexpected error {e:#?}"),
+        }
+    }
+}
