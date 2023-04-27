2cdc5fd0013387f6e91daeafe865e5c254deeb49
TheJokr <leo@bloecher.dev>
2023-04-27 23:23:36 +0200

    Add tests for name-value pair en-/decoding

--- src/protocol/nv.rs
+++ src/protocol/nv.rs
@@ -6,6 +6,7 @@ use super::Error as ProtocolError;
 
 /// An iterator decoding complete name-value pairs from its input.
 #[derive(Debug, Clone)]
+#[must_use = "iterators are lazy and do nothing unless consumed"]
 pub struct NVIter<'a> {
     data: &'a [u8],
 }
@@ -13,7 +14,6 @@ pub struct NVIter<'a> {
 impl<'a> NVIter<'a> {
     /// Creates a new [`NVIter`] over the referenced input bytes.
     #[inline]
-    #[must_use]
     pub fn new(data: &'a [u8]) -> Self {
         Self { data }
     }
@@ -58,4 +58,109 @@ pub fn write((name, value): (&[u8], &[u8]), mut w: impl Write) -> Result<usize, 
     w.write_all(name)?;
     w.write_all(value)?;
     Ok(written + name.len() + value.len())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    type NV<'a> = <NVIter<'a> as Iterator>::Item;
+
+    // Used in Params stream
+    const SHORT: NV = (b"GATEWAY_INTERFACE", b"CGI/1.1");
+    const SHORT_ENC: &[u8] = b"\x11\x07GATEWAY_INTERFACECGI/1.1";
+    // Exercises VarInt's long format
+    const LONG: NV = (
+        b"sTmRLkuxlXufZ#r?8UAN6GaUC=G|N@bU`d)H>^Z6!M~H~YM@5%~7KkTVr(6Z;MmQ}^C~l_OMxyDjQ&W$~p7G]=aYb\
+            GNIY:qr#&3<#{Y0ww76I[A>!'p*N+pSH@awPG[%,,LllGj*8KFT:{#+Fni4l=[ga][",
+        b"Chf=T+gf+YWUg/-sh.F6M;umM67rMy@ph4OPr`_84{s[iTB?di)5.,\\TuEoRwU!fVl__e#*lZgjp|J+CN+0vVJ8h\
+            'YQTV5|Z(aOuWN`12xk9a}:byqe'Tg*5IE3ex@$}{9;y_r)9a?_w(0ZKE(/O/KZ>\\g+SqZ>~",
+    );
+    const LONG_ENC: &[u8] = b"\x80\x00\x00\x9b\x80\x00\x00\xa0\
+        sTmRLkuxlXufZ#r?8UAN6GaUC=G|N@bU`d)H>^Z6!M~H~YM@5%~7KkTVr(6Z;MmQ}^C~l_OMxyDjQ&W$~p7G]=aYb\
+            GNIY:qr#&3<#{Y0ww76I[A>!'p*N+pSH@awPG[%,,LllGj*8KFT:{#+Fni4l=[ga][\
+        Chf=T+gf+YWUg/-sh.F6M;umM67rMy@ph4OPr`_84{s[iTB?di)5.,\\TuEoRwU!fVl__e#*lZgjp|J+CN+0vVJ8h\
+            'YQTV5|Z(aOuWN`12xk9a}:byqe'Tg*5IE3ex@$}{9;y_r)9a?_w(0ZKE(/O/KZ>\\g+SqZ>~";
+
+    // Used in GetValues payload
+    const NAME_ONLY: NV = (b"FCGI_MAX_CONNS", b"");
+    // Not used in FastCGI, but should be handled for robustness
+    const VALUE_ONLY: NV = (b"", b"'qZm*]*\"d^ig3p23k4L'");
+    const NV_PAIRS: [NV; 15] = [
+        NAME_ONLY,
+        VALUE_ONLY,
+        SHORT,
+        LONG,
+        (b"FCGI_MPXS_CONNS", b"1"),          // GetValuesResult
+        (b"FCGI_MAX_REQS", b"37589372"),     // GetValuesResult
+        (b"CONTENT_LENGTH", b"8382142"),     // Params stream
+        // Random bytes to test robustness
+        (b"}wnJ?^dLn.!cVfJ", b"'HWSwg,NP2md'"),
+        (b"T8'K", b"'ch(wconvA'"),
+        (b"WhKK7B`cL1j/X!*|l", b"'TAr@O%.^K`'"),
+        (b"{`0KJfv.T;E", b"'x/P5H[rRM,\"r'"),
+        (b"eO7=", b"'4}mixU_z6=N<-E<?uEWLK\\A{-'"),
+        (b"F`8-p", b"\"7Oo%=](kA@0OpN':\\WcL7x\""),
+        (b"\"wBC5,}bUbp<-RRw\\Lr[", b"'naY'"),
+        (b"_4jL04fo+$&b7!A", b"'0o/7==#MV{,wE[gTmNzf:@P'"),
+    ];
+
+    #[test]
+    fn write_spec() -> Result<(), ProtocolError> {
+        let mut buf = Vec::with_capacity(1000);
+        write(SHORT, &mut buf)?;
+        assert_eq!(buf, SHORT_ENC);
+
+        buf.clear();
+        write(LONG, &mut buf)?;
+        assert_eq!(buf, LONG_ENC);
+        Ok(())
+    }
+
+    #[test]
+    fn roundtrip() -> Result<(), ProtocolError> {
+        let mut buf = Vec::with_capacity(4000);
+        for &nv in &NV_PAIRS {
+            write(nv, &mut buf)?;
+        }
+
+        let mut orig_it = NV_PAIRS.iter();
+        let mut rt_it = NVIter::new(&buf);
+        for rt_nv in &mut rt_it {
+            let &orig_nv = orig_it.next()
+                .expect("NVIter returned too many elements");
+            assert_eq!(orig_nv, rt_nv);
+        }
+        assert!(matches!(orig_it.next(), None), "NVIter returned too few elements");
+
+        let rem = rt_it.into_inner();
+        assert_eq!(rem.len(), 0, "NVIter did not consume all input: {rem:?}");
+        Ok(())
+    }
+
+    #[test]
+    fn parse_invalid() -> Result<(), ProtocolError> {
+        parse_invalid_impl(SHORT)?;
+        parse_invalid_impl(LONG)
+    }
+
+    fn parse_invalid_impl(nv: NV) -> Result<(), ProtocolError> {
+        let mut buf = Vec::with_capacity(1000);
+        write(nv, &mut buf)?;
+        for len in [0, 1, buf.len() / 3, buf.len() / 2, buf.len().saturating_sub(5)] {
+            let trunc = &buf[..len];
+            let mut it = NVIter::new(trunc);
+            assert_eq!(it.next(), None);
+            assert_eq!(it.into_inner(), trunc);
+        }
+
+        let extend_by = buf.len() / 3;
+        buf.extend_from_within(..extend_by);
+        let mut it = NVIter::new(&buf);
+        assert_eq!(it.next(), Some(nv));
+        assert_eq!(it.next(), None);
+        assert_eq!(it.into_inner(), &buf[..extend_by]);
+        Ok(())
+    }
 }
