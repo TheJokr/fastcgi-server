8f7d2c2c97aebb966e735979c0f5957c95d80ed5
TheJokr <leo@bloecher.dev>
2023-05-02 17:17:52 +0200

    Add tests for Bytes extension trait

--- src/ext.rs
+++ src/ext.rs
@@ -43,3 +43,34 @@ impl Bytes for &mut [u8] {
         replace_with::replace_with_and_return(self, || &mut [], |s| s.split_at_mut(len))
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const REF: &[u8] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    #[test]
+    fn shared() {
+        let mut bytes = REF;
+        bytes.advance_by(20);
+        assert_eq!(bytes, &REF[20..]);
+
+        let head = bytes.split_head(7);
+        assert_eq!(head, &REF[20..27]);
+        assert_eq!(bytes, &REF[27..]);
+    }
+
+    #[test]
+    fn exclusive() {
+        let mut buf = REF.to_owned();
+        let mut bytes = &mut *buf;
+        bytes.advance_by(25);
+        assert_eq!(bytes, &REF[25..]);
+
+        let head = bytes.split_head(20);
+        assert_eq!(head, &REF[25..45]);
+        assert_eq!(bytes, &REF[45..]);
+    }
+}
