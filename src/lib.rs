4fa3fe821bfb763175514349695ea62dd9d78f6c
TheJokr <leo@bloecher.dev>
2023-05-05 18:02:34 +0200

    Add central Config struct in crate root

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@
 #![warn(clippy::pedantic, clippy::multiple_crate_versions)]
 #![allow(clippy::enum_glob_use, clippy::cast_possible_truncation, clippy::items_after_statements)]
 
+use std::num::NonZeroUsize;
+
 
 /// Helpful extension traits shared across the crate.
 pub(crate) mod ext;
@@ -17,3 +19,37 @@ pub mod protocol;
 
 // TODO(docs): Helpers for CGI/1.1 requests/responses
 pub mod cgi;
+
+
+/// The central configuration for [`fastcgi_server`](crate).
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Config {
+    /// The maximum number of concurrent FastCGI connections to the client.
+    ///
+    /// This parameter effectively defines the maximum amount of concurrency
+    /// this crate will exhibit. Each connection supports one request at a time
+    /// (that is, no multiplexing).
+    ///
+    /// The default is set to the OS's reported parallelism, such as the number
+    /// of available CPU cores. This is often appropriate for compute-bound
+    /// workloads with the FastCGI client on `localhost`. If using `async` APIs
+    /// or networked operations, a much higher number may be advisable.
+    pub max_conns: NonZeroUsize,
+}
+
+impl Config {
+    /// Create a new [`Config`] with all settings set to their documented defaults.
+    #[must_use = "Creating a Config is not free and has no side effects"]
+    pub fn new() -> Self {
+        let concurrency = std::thread::available_parallelism()
+            .unwrap_or(1.try_into().expect("fallback should be nonzero"));
+        Self { max_conns: concurrency }
+    }
+}
+
+impl Default for Config {
+    #[inline]
+    fn default() -> Self {
+        Self::new()
+    }
+}
