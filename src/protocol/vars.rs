ff46dbd73a758afd9dbfa3e542983bd27bdf4170
TheJokr <leo@bloecher.dev>
2023-05-06 20:54:11 +0200

    Add test for ProtocolVariables::write_response

--- src/protocol/vars.rs
+++ src/protocol/vars.rs
@@ -139,4 +139,26 @@ mod tests {
             Err(e) => panic!("returned unexpected error {e:#?}"),
         }
     }
+
+    #[test]
+    fn response_len() {
+        let vars = ProtocolVariables::all();
+        let max_config = Config { max_conns: usize::MAX.try_into().unwrap() };
+
+        let mut buf = Vec::with_capacity(ProtocolVariables::RESPONSE_LEN.next_power_of_two());
+        vars.write_response(&mut buf, &max_config);
+        assert_eq!(buf.len(), ProtocolVariables::RESPONSE_LEN);
+    }
+
+    #[test]
+    fn response() {
+        const REF: &[u8] = b"\x01\x0a\0\0\x00\x37\x01\0\x0e\x03FCGI_MAX_CONNS183\
+            \x0d\x03FCGI_MAX_REQS183\x0f\x01FCGI_MPXS_CONNS0\0";
+        let vars = ProtocolVariables::all();
+        let config = Config { max_conns: 183.try_into().unwrap() };
+
+        let mut buf = Vec::with_capacity(ProtocolVariables::RESPONSE_LEN);
+        vars.write_response(&mut buf, &config);
+        assert_eq!(buf, REF);
+    }
 }
