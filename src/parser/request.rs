6b2bada697f4d302893cd91964b5e56c199de80f
TheJokr <leo@bloecher.dev>
2023-05-10 17:40:29 +0200

    Add tests for completed Request

--- src/parser/request.rs
+++ src/parser/request.rs
@@ -87,3 +87,66 @@ impl Request {
         self.params.iter().map(|(k, v)| (k, v.as_ref()))
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::cgi::VarName;
+
+    use super::*;
+
+    const BYTES: &[u8] = b"\x1f\x9a\xdaM\xeb\x82U\xb8\xfe\xf4\xb0\xc7\x80\x95\xc6\
+        \xdf\xa3\xd3O,\xae\xa3\xa8x\x18@\x9a\xf7\x0f\xd6\x18\xbdv\x90\x80I\xa1\x99\xf8\xec";
+
+    const REF_ENV: &[(&str, &[u8])] = &[
+        ("GATEWAY_INTERFACE", b"CGI/1.1"),
+        ("CONTENT_LENGTH", b"67828"),
+        ("CONTENT_TYPE", b"text/plain"),
+        ("REQUEST_METHOD", b"HEAD"),
+        ("HTTP_DATE", b"Sun, 07 May 2023 19:42:27 GMT"),
+        ("HTTP_AuthORIZAtIon", b"Bearer xi/atccvRF7tN7p8J4Vw+KJ3AhikzBNhIBo0zQc7be5E"),
+        ("HTTP_x_unknown_test", b"Z+5ED\\SHGMN76&T}+fc%DE40@.jG"),
+        ("HTTP_X_NOT_UTF8", BYTES),
+        ("HTTP_X_FORWARDED_PROTO", b"https"),
+    ];
+
+    #[test]
+    fn env() {
+        let body = fcgi::body::BeginRequest{
+            role: fcgi::Role::Responder,
+            flags: fcgi::RequestFlags::all(),
+        };
+        let mut req = Request::new(1.try_into().unwrap(), body);
+        req.params.extend(REF_ENV.iter().map(
+            |&(n, v)| (n.into(), SmallBytes::from_slice(v))
+        ));
+
+        assert_eq!(req.env_len(), REF_ENV.len());
+        assert!(req.contains_var("request_method".into()));
+        assert!(req.contains_var(cgi::GATEWAY_INTERFACE.into()));
+        assert!(!req.contains_var("".into()));
+        assert!(!req.contains_var("kOHvQ!e&GROq&?0kz>=bQr`O`".into()));
+
+        assert!(matches!(req.get_var(cgi::CONTENT_LENGTH.into()), Some(b"67828")));
+        assert!(matches!(req.get_var("HTTP_X_NOT_UTF8".into()), Some(BYTES)));
+        assert!(matches!(req.get_var("".into()), None));
+        assert!(matches!(req.get_var("Y)tdz(".into()), None));
+
+        assert!(matches!(req.get_var_str(cgi::HTTP_X_FORWARDED_PROTO.into()), Some("https")));
+        assert!(matches!(req.get_var_str("HTTP_X_NOT_UTF8".into()), None));
+        assert!(matches!(req.get_var_str("".into()), None));
+        assert!(matches!(req.get_var_str("x9rJb03ASGg45".into()), None));
+
+        let mut it = req.env_iter();
+        let mut len = it.len();
+        assert_eq!(len, req.env_len());
+        for (n, v) in &mut it {
+            assert!(REF_ENV.iter().any(
+                |&(refn, refv)| <&VarName>::from(n) == <&VarName>::from(refn) && v == refv
+            ));
+            len -= 1;
+        }
+        assert_eq!(len, 0);
+        assert!(matches!(it.next(), None));
+    }
+}
