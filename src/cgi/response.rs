f4cba7f8a273f440b1b50a0b918c4a10166ecb29
TheJokr <leo@bloecher.dev>
2023-06-14 16:21:27 +0200

    Add tests for CGI/1.1 response helpers

--- src/cgi/response.rs
+++ src/cgi/response.rs
@@ -82,3 +82,101 @@ where
     w.write_all(b"\n\n")?;
     Ok(written + 2)
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn redirect() -> io::Result<()> {
+        const REF: &[&str] = &[
+            "/", "/3i-7gjb2-wetb409.html", "/example.html?foo=bar",
+            "http://example.com", "https://example.com/foo?q=bar#baz",
+            "https://user@example.org/a1zs5/bdg834/cj94k/dh78f?njejk=572#frag",
+        ];
+
+        let mut buf = Vec::with_capacity(1024);
+        for dest in REF {
+            buf.clear();
+            let exp = format!("Location: {dest}\n\n");
+            let len = simple_redirect(&mut buf, dest)?;
+            assert_eq!(len, buf.len());
+            assert_eq!(buf, exp.as_bytes());
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn doc_response() -> io::Result<()> {
+        const REF: &[u8] = b"Status: 200 OK\nContent-Type: text/plain; charset=utf-8\n\
+            Etag: TebWmVZhLynbmkSaxnwq\nServer: fastcgi-server\n\n";
+        const HEADERS: &[(&[u8], &[u8])] = &[
+            (b"Content-Type", b"text/plain; charset=utf-8"),
+            (b"Etag", b"TebWmVZhLynbmkSaxnwq"),
+            (b"Server", b"fastcgi-server"),
+        ];
+        check_headers(http::StatusCode::OK, HEADERS, REF)
+    }
+
+    #[test]
+    fn custom_redirect() -> io::Result<()> {
+        const REF: &[u8] = b"Status: 307 Temporary Redirect\n\
+            Location: https://example.com/foo.html?bar=baz#frag\n\
+            Cache-Control: public, max-age=18682\n\
+            Expires: Tue, 13 Jun 2023 12:04:19 GMT\n\n";
+        const HEADERS: &[(&[u8], &[u8])] = &[
+            (b"Location", b"https://example.com/foo.html?bar=baz#frag"),
+            (b"Cache-Control", b"public, max-age=18682"),
+            (b"Expires", b"Tue, 13 Jun 2023 12:04:19 GMT"),
+        ];
+        check_headers(http::StatusCode::TEMPORARY_REDIRECT, HEADERS, REF)
+    }
+
+    #[test]
+    fn custom_status() -> io::Result<()> {
+        const REF: &[u8] = b"Status: 999 Custom\nContent-Type: application/json\n\
+            Content-Encoding: brotli\nLast-Modified: Tue, 13 Jun 2023 16:41:40 GMT\n\n";
+        const HEADERS: &[(&[u8], &[u8])] = &[
+            (b"Content-Type", b"application/json"),
+            (b"Content-Encoding", b"brotli"),
+            (b"Last-Modified", b"Tue, 13 Jun 2023 16:41:40 GMT"),
+        ];
+        let status = http::StatusCode::from_u16(999).unwrap();
+        check_headers(status, HEADERS, REF)
+    }
+
+    fn check_headers(
+        status: http::StatusCode,
+        headers: &[(&[u8], &[u8])],
+        exp: &[u8],
+    ) -> io::Result<()> {
+        let mut buf = Vec::with_capacity(1024);
+        let len = write_headers(&mut buf, status, headers.iter().copied())?;
+        assert_eq!(len, buf.len());
+        assert_eq!(buf, exp);
+        Ok(())
+    }
+
+    #[test]
+    fn http_response() -> io::Result<()> {
+        const REF: &[u8] = b"Status: 503 Service Unavailable\n\
+            content-type: text/html; charset=utf-8\n\
+            date: Tue, 13 Jun 2023 17:02:36 GMT\nvary: accept\n\
+            link: <https://example.com>; rel=\"preconnect\"\n\n";
+        let resp = http::response::Builder::new()
+            .status(503)
+            .header(http::header::CONTENT_TYPE, "text/html; charset=utf-8")
+            .header(http::header::DATE, "Tue, 13 Jun 2023 17:02:36 GMT")
+            .header(http::header::VARY, "accept")
+            .header(http::header::LINK, "<https://example.com>; rel=\"preconnect\"")
+            .body(())
+            .unwrap();
+
+        let mut buf = Vec::with_capacity(1024);
+        let len = http_headers(&mut buf, &resp)?;
+        assert_eq!(len, buf.len());
+        assert_eq!(buf, REF);
+        Ok(())
+    }
+}
