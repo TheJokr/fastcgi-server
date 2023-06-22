cfeff65f9cdea262071412a4224031efd60ae43e
TheJokr <leo@bloecher.dev>
2023-06-22 14:38:58 +0200

    Reuse dev profile outputs for E2E test binaries

--- ci/test-e2e.sh
+++ ci/test-e2e.sh
@@ -10,7 +10,7 @@ sudo -n apt-get -qy install --no-install-recommends nginx-core apache2 lighttpd
 echo '::endgroup::'
 
 echo '::group::Build FastCGI examples'
-cargo build --profile e2e-test --examples --all-features
+cargo build --examples --all-features
 echo -e '::endgroup::\n'
 
 declare -A servers=(
@@ -20,7 +20,7 @@ declare -A servers=(
 )
 for srv in "${!servers[@]}"; do
     echo "::group::Test with ${srv}"
-    RUST_LOG=trace target/e2e-test/examples/hello-cgi 2> "e2e-logs/hello-cgi-${srv}.log" &
+    RUST_LOG=trace target/debug/examples/hello-cgi 2> "e2e-logs/hello-cgi-${srv}.log" &
     ${servers["$srv"]} 2> "e2e-logs/${srv}.log" &
     newman run ci/e2e.postman_collection --env-var 'base_url=localhost:8080' \
         --color on --timeout 600000
